9194bb8b3741837691dbb1b260cc94270c250199
lenton <mrlongislandicetea@hotmail.com>
2016-12-29 12:56:13 +1300

    &desc

--- scripts/git_update.sh
+++ scripts/git_update.sh
@@ -2,11 +2,26 @@
 
 cd /home/lneale/SHConfig
 
-read -p " Commit description: " desc
+read -p " Commit description:(SHConfig) " desc
 git add . && \
 git add -u && \
 git commit -m "&desc" && \
 git push
 
 
-read
+cd /home/lneale/Project_Files/CyanoPi
+
+read -p "Commit description:(CyanoPi)" desc
+git add . && \
+git add -u && \
+git commit -m "&desc" && \
+git push
+
+
+cd /home/lneale/Project_Files/CyanoDocs
+
+read -p "Commit description:(CyanoDocs) " desc
+git add . && \
+git add -u && \
+git commit -m "&desc" && \
+git push
